acd0df201e67af08448d33df443b0dddd0d53a54
SyuyaMurakami <89246518+SyuyaMurakami@users.noreply.github.com>
2026-06-09 23:10:18 +0800

    Initiate

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Revision history for RiskQuantLib
+
+## 0.1.34.0 -- 2026-06-09
+
+* First version.
---
+++ src/RiskQuantLib/AttributeKey.hs
@@ -0,0 +1,41 @@
+{-# LANGUAGE BangPatterns #-}
+
+module RiskQuantLib.AttributeKey (
+  AttrName,
+  AttrIndex,
+  toAttr
+) where
+
+import System.IO.Unsafe (unsafePerformIO)
+import qualified Data.IORef as IOR
+import qualified Data.Text as T
+import qualified Data.Map.Strict as SM
+
+type AttrName = String
+type AttrKey = T.Text
+type AttrIndex = Int
+type AttrMap = IOR.IORef (SM.Map AttrKey AttrIndex)
+
+newAttrMap :: IO AttrMap
+newAttrMap = IOR.newIORef SM.empty
+
+modifyAttrMap :: AttrMap -> AttrKey -> IO AttrIndex
+modifyAttrMap attrMap attrKey = IOR.atomicModifyIORef' attrMap $ \am -> 
+  case SM.lookup attrKey am of
+    Just v -> (am, v)
+    Nothing -> let !sz = SM.size am in (SM.insert attrKey sz am, sz)
+
+fromAttrMap :: AttrMap -> AttrName -> IO AttrIndex
+fromAttrMap attrMap attrName = IOR.readIORef attrMap >>= \am -> 
+  let !an = T.pack attrName in case SM.lookup an am of
+    Just v -> return v
+    Nothing -> modifyAttrMap attrMap an
+
+{-# NOINLINE globalSymbolTable #-}
+
+globalSymbolTable :: AttrMap
+globalSymbolTable = unsafePerformIO $ newAttrMap
+
+toAttr :: AttrName -> AttrIndex
+toAttr attrName = unsafePerformIO $ fromAttrMap globalSymbolTable attrName
+
